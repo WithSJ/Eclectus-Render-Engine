ef74d30502f252c5b912c4c9129a4ec9fc924a54
Sandeep Jadam <withsj.in@gmail.com>
2021-04-13 21:44:37 +0530

    Delete moved files in there folders

--- utils.py
+++
@@ -1,6 +0,0 @@
-def wrap_func(lib,funcname,restype,argtypes):
-    """ Wrapping ctypes functions"""
-    func = lib.__getattr__(funcname)
-    func.restype = restype
-    func.argtypes = argtypes
-    return func
