7b9507647fbb74556ef3e0fd00fcf4c5ebf86227
Sandeep Jadam <withsj.in@gmail.com>
2021-04-15 22:51:22 +0530

    Color from_hex comment added

--- CPP/Engine/color_lib.cpp
+++ CPP/Engine/color_lib.cpp
@@ -13,18 +13,20 @@ public:
     }
 
     static Color from_hex(std::string hex_str)
-    {
-       std::string R="";
-       R = R + hex_str[1] + hex_str[2];
-       
-       std::string G="";
-       G = G + hex_str[3] + hex_str[4];
-       
-       std::string B="";
-       B = B + hex_str[5] + hex_str[6];
+    {   //Create Color RGB using color hex string 
+        // string should be start from '#' (#ffffff)
 
-       Color new_col(hexToInt(R),hexToInt(G),hexToInt(B));
-       return  new_col;
+        std::string R="";
+        R = R + hex_str[1] + hex_str[2];
+        
+        std::string G="";
+        G = G + hex_str[3] + hex_str[4];
+        
+        std::string B="";
+        B = B + hex_str[5] + hex_str[6];
+
+        Color new_col(hexToInt(R),hexToInt(G),hexToInt(B));
+        return  new_col;
     }
 };
 
