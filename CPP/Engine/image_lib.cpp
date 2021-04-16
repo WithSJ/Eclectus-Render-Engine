ac68048fb388be6db81cc0308ad23e84c58dfa4d
Sandeep Jadam <withsj.in@gmail.com>
2021-04-16 22:34:21 +0530

    optamize in to short int memory

--- CPP/Engine/image_lib.cpp
+++ CPP/Engine/image_lib.cpp
@@ -5,15 +5,15 @@ class Image
     private:
         /* data */
     public:
-        int HEIGTH=0, WIDTH=0;
+        short int HEIGTH=0, WIDTH=0;
         Color** PIXCELS;
         
-        Image(int height, int width)
+        Image(short int height, short int width)
         {   this->HEIGTH = height; // Assigen Height and Width
             this->WIDTH = width;
             
             this->PIXCELS = new Color* [WIDTH]; // Create pixcels accorfing Height and Width
-            for (int i = 0; i < WIDTH; i++)
+            for (short int i = 0; i < WIDTH; i++)
             {
                 this->PIXCELS[i] = new Color [HEIGTH];
             }
