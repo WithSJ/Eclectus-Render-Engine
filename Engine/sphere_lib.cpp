340efd116451726a3d495ea0ba659fc8b405d15b
Sandeep Jadam <withsj.in@gmail.com>
2021-04-20 23:09:41 +0530

    sphere intersects method added

--- Engine/sphere_lib.cpp
+++ Engine/sphere_lib.cpp
@@ -1,5 +1,8 @@
+#include<math.h>
+
 #include "vector_lib.cpp"
 #include "color_lib.cpp"
+#include "ray_lib.cpp"
 
 class Sphere
 {
@@ -13,5 +16,28 @@ class Sphere
             this->Center = center;
             this->Radius = radius;
             this->Material = material;
+        }
+
+        float intersects(Ray ray)
+        {
+            Vector sphere_to_ray = ray.Origin - this->Center;
+
+            float a = 1.0;
+
+            float b = 2 * ray.Direction.dot_product(sphere_to_ray);
+
+            float c = sphere_to_ray.dot_product(sphere_to_ray) - this->Radius * this->Radius;
+
+            float discriminant = b * b - 4 * a * c;
+
+            if (discriminant >= 0)
+            {
+                float distance = (-b - sqrt(discriminant)) / 2;
+                if(distance>0)
+                    return distance;
+            }
+
+            return NULL;
+
         }
 };
