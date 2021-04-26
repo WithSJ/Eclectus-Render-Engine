dc6d4def310c37f6d3e8c5d09f6fee44fb6e9ddb
Sandeep Jadam <withsj.in@gmail.com>
2021-04-26 22:51:34 +0530

    MAIN CODE this is how we can render

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
+#include "Engine/renderengine_lib.cpp"
+
 int main(int argc, char const *argv[])
 {
     /* code */
+    // Eclectus Testing 1.0
+
+    short int WIDTH = 320;
+    short int HEIGHT = 200;
+    
+    Vector Camera(0,0,-1);
+    Color col(255,0,0);
+    Sphere obj1(Vector(0,0,0),0.5,col);
+    
+    Sphere Objects = {obj1};
+    Scene scene(Camera,&Objects,WIDTH,HEIGHT);
+
+    RenderEngine engine;
+
+    Image img =  engine.render(scene);
+
+    img.write_ppm("Eclectus_test.ppm");
+    
     return 0;
 }
