25142e2c21840b6b472ba30c2acbe186a21b82e3
Sandeep Jadam <withsj.in@gmail.com>
2021-04-29 19:03:01 +0530

    Lights  use in Scene class

--- Engine/scene.h
+++ Engine/scene.h
@@ -2,6 +2,7 @@
 #define SCENE_H
 
 #include "sphere.h"
+#include "light.h"
 
 class Scene
 {
@@ -12,13 +13,17 @@ class Scene
         Vector Camera;
         Sphere* Objects; 
         short int NumberOfObjects;
+        Light* Lights;
+        short int NumberOfLigths;
         short int Width, Height;
         
-        Scene(Vector camera, Sphere objects[], short int number_of_objects, short int width, short int height)
+        Scene(Vector camera, Sphere objects[], short int number_of_objects, Light lights[],short int number_of_ligths,short int width, short int height)
         {
             this->Camera = camera;
             this->Objects = objects;
             this->NumberOfObjects = number_of_objects;
+            this->Lights = lights;
+            this->NumberOfLigths = number_of_ligths;
             this->Width = width;
             this->Height = height;
         }
