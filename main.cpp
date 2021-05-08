fc96d0f7683b0bc90002470b14aafae6d2ae4f41
Sandeep Jadam <withsj.in@gmail.com>
2021-05-08 21:10:41 +0530

    main.cpp update

--- main.cpp
+++ main.cpp
@@ -14,21 +14,33 @@ int main(int argc, char const *argv[])
     short int WIDTH = 320;
     short int HEIGHT = 200;
     
-    Vector Camera(0,0,-1);
-    Color col = Color::from_hex("#ff751a");
-    Material material1(col);
-    Sphere obj1(Vector(0,0,0),0.3,material1);
+    Vector Camera(0, 0, -1);
 
-    Color col2 = Color::from_hex("#9fff80");
-    Material material2(col2);
-    Sphere obj2(Vector(0,5,0),5,material2);
-    Sphere Objects[] = {obj1,obj2};
+    Color col = Color::from_hex("#0000ff");
+    Material material1(col);
+    Sphere obj1(Vector(-0.75, -0.1, 2.25),0.6,material1);
+
+    Color col3 = Color::from_hex("#803880");
+    Material material3(col3);
+    Sphere obj3(Vector(0.75, -0.1, 1) ,0.6,material3);
+
+    // Ground
+    Color Gcol = Color::from_hex("#00ff00");
+    Material material2(Gcol);
+    Sphere obj2(Vector(0,10000,0),10000,material2);
+    
+    
+    Sphere Objects[] = {obj1,obj2,obj3};    
     
     Color light_col(255,255,255);
-    Light point_light(Vector(1.5,-0.5,-3.0),light_col);
-    Light Lights[] = {point_light};
+    Light point_light(Vector(1.5, -0.5, -10),light_col);
+    
+    Color light_col2 = Color::from_hex("#E6E6E6");
+    Light point_light2(Vector(-0.5, -10.5, 0),light_col2);
 
-    Scene scene(Camera,Objects,2,Lights,1,WIDTH,HEIGHT);
+    Light Lights[] = {point_light,point_light2};
+
+    Scene scene(Camera,Objects,3,Lights,2,WIDTH,HEIGHT);
 
     RenderEngine engine;
 
